49b81a013c046c894737ceb369ac0b0438b49cc1
Asanka <asankalakmal@gmail.com>
2012-12-13 13:37:26 +0800

    Add 2 functions for the generate string

--- logicGenerator.php
+++ logicGenerator.php
@@ -1,6 +1,36 @@
 <?php
 
 Class logicGenerator {
-
+	
+	public $final_result=null;
+	public function __construct (){}
+	
+	public function resultGenerate ($val)
+	{
+		$is_printed=false;
+		$result=null;
+		if( $val%3 == 0 ) {
+			$result='fuzz';
+			$is_printed=true;
+		}
+		if( $val%5 == 0 ) {
+			$result.='buzz';
+			$is_printed=true;
+		}
+		
+		if( !$is_printed ) {
+			$result=$val;
+		}
+		return $result;
+	}
+	
+	public function logicGenerate ($val1,$val2)
+	{
+		for( $i=$val1; $i<=$val2; $i++ )
+		{
+			$this->final_result.=$this->resultGenerate ($i).'  ';
+		}
+		return $this->final_result;
+	}
 
 }
