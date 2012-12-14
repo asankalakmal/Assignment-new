0cd6a428f4d447f1f2f2f4ab89b5fc1d921a43a3
Asanka <asankalakmal@gmail.com>
2012-12-14 17:41:17 +0800

    modify the logic in resultGenerate function

--- logicGenerator.php
+++ logicGenerator.php
@@ -12,6 +12,8 @@
 Class logicGenerator {
 	
 	public $final_result=null;// Final result 
+	public $fuzz_print=false;
+	public $buzz_print=false;
 	public function __construct (){}
 	
 	/**
@@ -23,15 +25,22 @@ Class logicGenerator {
 	{
 		$is_printed=false;
 		$result=null;
-		if( $val%3 == 0 ) {// if value multiple of 3 then 
-			if( $val%5 != 0 ) {// if variable not a multiple of 5 then result is fuzz
-				$result='fuzz';
-				$is_printed=true;
-			}
+		if( $val%3 == 0 ) {// if value multiple of 3 then result is fuzz
+			$result='fuzz';
+			$is_printed=true;
+			$this->fuzz_print=true;
 		}
 		if( $val%5 == 0 ) {// if value multiple of 5 then result is buzz
 			$result.='buzz';
 			$is_printed=true;
+			$this->buzz_print=true;
+		}
+		// if value not a multiple of 5 or 3 and consecutive Fizzes/Buzzes then result is buzz
+		if( ($this->buzz_print && $this->fuzz_print) &&  ( $val%5 != 0 && $val%3 != 0) ) {
+			$result='buzz';
+			$is_printed=true;
+			$this->buzz_print=false;
+			$this->fuzz_print=false;
 		}
 
 		if( !$is_printed ) {// if value is not a multiple of 5 or 3 result equal to value
